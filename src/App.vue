323971c36f8480f6ee247e4709f5541a19b8c127
Tippy <tippy_q@163.com>
2022-03-03 21:40:30 +0800

    解决直接进入时不使用主题css的问题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
+import Cookie from "js-cookies"
+import {onMounted} from "vue";
+import {useTheme} from "@/utils/themes";
 
+onMounted(() => {
+  //设置主题
+  useTheme(Cookie.getItem('site_theme') || 'default')
+})
 </script>
 
 <template>
